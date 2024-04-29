51282da99fafd7a8d42d9521239e736882e1f581
LuizArt9 <166402385+LuizArt9@users.noreply.github.com>
2024-04-29 20:10:07 -0300

    Add files via upload

---
+++ atividade 1.py
@@ -0,0 +1,15 @@
+import pythonds.basic.stack
+
+def convert(deci):
+    resto = Stack()
+    while deci > 0:
+        rem = deci % 2
+        resto.push(rem)
+        deci = deci // 2
+    bin = ""
+    while not resto.isEmpty():
+        bin = bin + str(resto.pop())
+    return bin
+
+print("os números convertidos são 32, 57, 101 e 77")
+print(convert(32), convert(57), convert(101), convert(77), sep="")
